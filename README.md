e3d8a1f9bfee003e529710ff0db475e38cd74982
strawstack <rhshadowgreen@gmail.com>
2022-11-27 18:51:49 -0800

    Create grid

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # CoinCard
 
 A simple game built with HTML and JS animated with CSS.
+
+# Rules
+
+- Player starts with one special "bank" machine in the center. This machine is originaly manual.
+
+- Cells are Numbers, Open, or Machines.
+
+- Open cells can be purchased at which time they become machines.
+
+- Click the outer circles of a machine to "mine" manually.
+
+- "mine" means reach to adj cells and pull as much values as possible such that mining machine does not excees 9 coins.
+
+- Automine can be purchased and toggled by pressing the center of a machine to open a menu.
+
+- Menu shows over top of lower right or left of the game grid depending on what machine is selected.
+
+- Player can sell a machine for the original cost of purchasing the cell plus extra if they bought automine. The cell will become Open again in that case.
+
+- Total money that player can spend is shown in the center of the bank machine.
+
+- Total money can be raised by bringing coins to this machine or selling machines.
+
+- If a machine is in auto mine state player must still pick a direction. The machine will automine in this direction until further mining would exceed the 9 coin limit or there are no coins left to mine in the adj cell.
+
+- Click center of machine to show menu. Menu contains:
+    - Purchase automine.
+    - Toggle automine.
+    - Sell machine.
+    - Perhaps buy a single speed upgrade?
+
+# TODO
+
+- [ ] Finish generating the 2D grid array (placeholders until Machine class is built).
+
+- [ ] Create class for Machines
+    - [ ] uid, arm x 4, coins, cell {row, col}, mine function, action state (boolean).
+
+- [ ] Special center machine can hold 999+ coins (storage capacity is only special).
--- script.js
+++ script.js
@@ -1,5 +1,106 @@
 (() => {
 
-    console.log("hello");
+    function p(text) {
+        console.log(text);
+    }
+
+    function getDocumentBodySize() {
+        const WIDTH  = window.innerWidth;
+        const HEIGHT = window.innerHeight;
+        return {
+            WIDTH, HEIGHT
+        };
+    }
+
+    function getDocumentBodySizeAsMultipleOf(CELL_SIZE) {
+        const { WIDTH, HEIGHT } = getDocumentBodySize();
+        const ROWS = Math.floor( HEIGHT / CELL_SIZE );
+        const COLS = Math.floor( WIDTH / CELL_SIZE );
+        return {
+            WIDTH: COLS * CELL_SIZE,
+            HEIGHT: ROWS * CELL_SIZE,
+            ROWS: ROWS,
+            COLS: COLS
+        };
+    }
+
+    function makeLine(x1, y1, x2, y2) {
+        const svgns = "http://www.w3.org/2000/svg";
+        let line = document.createElementNS(svgns, "line");
+        line.setAttribute("x1", x1);
+        line.setAttribute("y1", y1);
+        line.setAttribute("x2", x2);
+        line.setAttribute("y2", y2);
+        return line;
+    }
+
+    function drawGridLines(svg, CELL_SIZE, WIDTH, HEIGHT, COLS, ROWS) {
+        
+        let line = makeLine(0, 1, WIDTH, 1);
+        line.setAttribute("class", "gridLine");
+        svg.appendChild(line);
+
+        svg.appendChild(line);
+        for (let r = 0; r < ROWS; r++) {
+            let line = makeLine(0, r * CELL_SIZE - 1, WIDTH, r * CELL_SIZE - 1);
+            line.setAttribute("class", "gridLine");
+            svg.appendChild(line);
+        }
+
+        line = makeLine(0, HEIGHT - 1, WIDTH, HEIGHT - 1);
+        line.setAttribute("class", "gridLine");
+        svg.appendChild(line);
+
+        line = makeLine(1, 0, 1, HEIGHT);
+        line.setAttribute("class", "gridLine");
+        svg.appendChild(line);
+
+        for (let c = 0; c < COLS; c++) {
+            let line = makeLine(c * CELL_SIZE - 1, 0, c * CELL_SIZE - 1, HEIGHT);
+            line.setAttribute("class", "gridLine");
+            svg.appendChild(line);
+        }
+
+        line = makeLine(WIDTH - 1, 0, WIDTH - 1, HEIGHT);
+        line.setAttribute("class", "gridLine");
+        svg.appendChild(line);
+    }
+
+    function makeGrid(WIDTH, HEIGHT, ROWS, COLS) {
+        let grid = [];
+
+        /* A cell can have the following states:
+         *  Number: contains a number 0 to 9.
+         *  Machine: contains a machine.
+         *  Open: the player can buy the cell at which time it will become a machine.
+         */
+
+        
+    }
+
+    function main() {
+
+        // Constants
+        const CELL_SIZE = 80;
+        const { WIDTH, HEIGHT, ROWS, COLS } = getDocumentBodySizeAsMultipleOf(CELL_SIZE);
+
+        // Elements
+        const svg = document.querySelector("svg");
+
+        // Variables
+        let grid = makeGrid(WIDTH, HEIGHT, ROWS, COLS);
+        
+        // Set SVG to fill screen
+        svg.setAttribute("width", WIDTH);
+        svg.setAttribute("height", HEIGHT);
+
+        // Draw grid lines
+        drawGridLines(svg, CELL_SIZE, WIDTH, HEIGHT, COLS, ROWS);
+
+        p(grid)
+
+    }
+
+    main();
 
 })();
